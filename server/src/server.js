27d45804030123f14c24f961c49ddd6b9b0ffa89
Erick dos Santos <erickspy2003@gmail.com>
2023-10-30 15:39:57 -0300

    feat: crud message

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,63 @@
-import fastify from 'fastify'
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
 
-const app = fastify()
+const prisma = new PrismaClient()
+
+const app = Fastify({
+  logger: true
+})
+
+app.post('/message', async (request, response) => {
+  const { title, content } = request.body
+
+  await prisma.message.create({
+    data: {
+      id: randomUUID(),
+      title,
+      content
+    }
+  })
+
+  return response.status(201).send('create')
+})
+
+app.get('/message', async (request, response) => {
+  const message = await prisma.message.findMany()
+
+  return { message }
+})
+
+app.put('/message/:id', async (request, response) => {
+  const { title, content } = request.body
+  const { id } = request.params
+
+  await prisma.message.update({
+    where: {
+      id
+    },
+    data: {
+      title,
+      content
+    }
+  })
+
+  return response.status(204).send()
+})
+
+app.delete('/message/:id', async (request, response) => {
+  const { id } = request.params
+
+  await prisma.message.delete({
+    where: {
+      id
+    }
+  })
+})
 
 
 app.listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log(`HTTP Server running on http://localhost:3333`)
-  })
+  port: 3333,
+}).then(() => {
+  console.log(`HTTP Server running on http://localhost:3333`)
+})
